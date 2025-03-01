9d2164b95fcdc99cd23c447894058452442e130c
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2025-03-01 13:52:13 +0530

    Updated Code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,30 +11,33 @@ const registerUser = async (req, res) => {
   // check for user creation
   // return res
   const { username, phone, email, password } = req.body;
-  // if (
-  //   [username, phone, email, password].some((fields) => fields?.item === "")
-  // ) {
-  //   return res.status(400).json({ error: "Please fill all fields" });
-  // }
+  if (
+    [username, phone, email, password].some((fields) => fields?.item === "")
+  ) {
+    return res.status(400).json({ error: "Please fill all fields" });
+  }
 
-  // const existedUser = await User.findOne({ email });
-  // if (existedUser) {
-  //   return res.status(400).json({ error: "Email already exists" });
-  // }
-  // console.log(req.file, "received");
-  // const avatarLocalPath = req.file?.path;
-  // if (!avatarLocalPath)
-  //   return res
-  //     .status(404)
-  //     .json({ success: false, message: " Avatar not found" });
-  // const avatarCloudinaryUrl = await uploadOnCloudinary(avatarLocalPath);
-  // if (!avatarCloudinaryUrl){
-  //   return res
-  //    .status(500)
-  //    .json({ success: false, message: "Failed to upload avatar to cloudinary" });
-  // }
-  // console.log(avatarCloudinaryUrl, "upload successful");
-  // return res.status(200).json({ success: true, message: "okku!" });
+  const existedUser = await User.findOne({ email });
+  if (existedUser) {
+    return res.status(400).json({ error: "Email already exists" });
+  }
+  console.log(req.file, "received");
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath)
+    return res
+      .status(404)
+      .json({ success: false, message: " Avatar not found" });
+  const avatarCloudinaryUrl = await uploadOnCloudinary(avatarLocalPath);
+  if (!avatarCloudinaryUrl) {
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: "Failed to upload avatar to cloudinary",
+      });
+  }
+  console.log(avatarCloudinaryUrl, "upload successful");
+  return res.status(200).json({ success: true, message: "okku!" });
 };
 
 export { registerUser };
